1d2047fa98424ac2d7e208d6f7c7e80b5c93e3c6
sqrmyy <dn41.ra20adtdjw@gmail.com>
2022-01-07 20:42:12 +0900

    fix 4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero'
-import { HeroService } from '../hero.service'; 
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,7 +14,10 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = []
   selectedHero?: Hero
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
     this.getHeroes()
@@ -21,6 +25,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero) {
     this.selectedHero = hero
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
