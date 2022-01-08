ff67d67f75dd212e1f4d5ad133e9880c8192d530
sqrmyy <dn41.ra20adtdjw@gmail.com>
2022-01-08 15:12:06 +0900

    fix 6

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero'
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,8 +13,7 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = []
 
   constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
+    private heroService: HeroService
   ) { }
 
   ngOnInit(): void {
@@ -24,6 +22,17 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+  }
+
+  add(name: string): void {
+    name = name.trim()
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero).subscribe(hero => { this.heroes.push(hero)})
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
 
 }
